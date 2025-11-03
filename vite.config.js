aa559b24f75a61906852d20d597751528b0faf4d
tyoprataama <tyoprataama004@gmail.com>
2025-11-03 12:45:03 +0700

    update:homepage for root domain deployment

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-// ✅ karena __dirname tidak tersedia di ESM, kita definisikan manual
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: '/portfolio',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
